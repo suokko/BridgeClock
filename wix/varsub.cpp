00df9b5b355811e4e259a6332c316012341f1d20
Pauli Nieminen <suokkos@gmail.com>
2014-02-05 17:48:28 +0200

    msi package generation
    Signed-off-by: Pauli Nieminen <suokkos@gmail.com>

---
+++ wix/varsub.cpp
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <string>
+#include <map>
+
+typedef std::map<std::string, std::string> stringmap;
+
+int main(void)
+{
+	int state = 0;
+	int c;
+	std::string name, var;
+	stringmap vars;
+
+	while ((c = getchar()) != EOF) {
+		if (c == '\r')
+			continue;
+		switch (state) {
+		case 0:
+			if (c == '\n')
+				name = "";
+			else if (c == 0xad) {
+				getchar();
+				goto nextfile;
+			} else if (c != '=')
+				name += c;
+			else {
+				var = "";
+				state = 1;
+			}
+			break;
+		case 1:
+			if (c != '\n')
+				var += c;
+			else {
+				state = 0;
+				vars[name] = var;
+				name = "";
+			}
+		}
+	}
+
+nextfile:
+
+	while ((c = getchar()) != EOF) {
+		if (c == '\r')
+			continue;
+		switch (state) {
+		case 0:
+			if (c == '$')
+				state = 1;
+			else
+				putchar(c);
+			break;
+		case 1:
+			if (c == '{') {
+				name = "";
+				state = 2;
+			} else {
+				putchar('$');
+				putchar(c);
+				state = 0;
+			}
+			break;
+		case 2:
+			if (c != '}')
+				name += c;
+			else {
+				if (vars.count(name) == 0) {
+					fprintf(stderr, "%s not found in defined variables\n", name.c_str());
+					return 2;
+				} else
+					printf("%s", vars[name].c_str());
+				state = 0;
+			}
+			break;
+		}
+	}
+	return 0;
+}

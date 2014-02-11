53b7ec22325bc82ec2ada82e39af81092218fb86
Pauli Nieminen <suokkos@gmail.com>
2014-02-11 13:47:24 +0200

    Include locale files releases
    Signed-off-by: Pauli Nieminen <suokkos@gmail.com>

--- locale/BridgeClock_en.ts
+++ locale/BridgeClock_en.ts
@@ -1,4 +1,48 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
+<context>
+    <name>tournamentStart</name>
+    <message>
+        <location filename="../qml/BridgeClock/tournamentStart.qml" line="+35"/>
+        <source>Kilpailun pituus</source>
+        <translation>Number of rounds</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Kierroksia</source>
+        <translation>Rounds</translation>
+    </message>
+    <message>
+        <location line="+36"/>
+        <source>Kierrosaika</source>
+        <translation>Round time</translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <location line="+43"/>
+        <source>minuuttia</source>
+        <translation>minutes</translation>
+    </message>
+    <message>
+        <location line="-18"/>
+        <source>Vaihtoaika</source>
+        <translation>Change time</translation>
+    </message>
+    <message>
+        <location line="+43"/>
+        <source>Alkamisaika</source>
+        <translation>Start time</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Nyt</source>
+        <translation>Now</translation>
+    </message>
+    <message>
+        <location line="+30"/>
+        <source>Aloita uusi kilpailu</source>
+        <translation>Start a new competition</translation>
+    </message>
+</context>
 </TS>
--- locale/BridgeClock_sv.ts
+++ locale/BridgeClock_sv.ts
@@ -1,4 +1,48 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="sv_SE">
+<context>
+    <name>tournamentStart</name>
+    <message>
+        <location filename="../qml/BridgeClock/tournamentStart.qml" line="+35"/>
+        <source>Kilpailun pituus</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Kierroksia</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+36"/>
+        <source>Kierrosaika</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <location line="+43"/>
+        <source>minuuttia</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-18"/>
+        <source>Vaihtoaika</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+43"/>
+        <source>Alkamisaika</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Nyt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+30"/>
+        <source>Aloita uusi kilpailu</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>

af314c91d3ce92d6c7846eb5300de38e3a3febb0
Pauli Nieminen <suokkos@gmail.com>
2014-02-12 11:06:03 +0200

    Fix before start pausing
    Signed-off-by: Pauli Nieminen <suokkos@gmail.com>

--- timemodel.cpp
+++ timemodel.cpp
@@ -551,7 +551,7 @@ void TimeModel::setPaused(bool v)
         QDateTime end;
         if (row + 1 != (int)list_.size()) {
             int elapsed = pauseTime_.elapsed();
-            end = item[1].start_;
+            end = item[0].start_;
             end = end.addMSecs(elapsed);
             changeEnd(row, end);
         }
